4a3c1b5891d62f8d29c1805b342a880615891b0c
LewisB321 <107862880+LewisB321@users.noreply.github.com>
2023-01-16 17:35:46 +0000

    Add files via upload

--- https-ciphers.sh
+++ https-ciphers.sh
@@ -2,12 +2,42 @@
 
 echo_results() {
 	
+	#This function will only be called by the below function and not in the main script
 	grade=$(echo $full_list | grep -w "least strength" | awk '{print $4}')
-	echo -e "\nThe encryption strength of the remote host is: " $grade
+	echo -e "\nThe encryption strength of the remote host is:" $grade
 	echo "This grade represents the security strength of the weakest cipher algorithm in all available cipher suites"
-	echo "For a full list of returned output, PLACEHOLDER"	
+	echo "The grading system used is extracted from the SSL Labs Server Rating Guide, available at https://www.ssllabs.com/projects/rating-guide"
+	echo -e "For a full list of returned output, Please refer to the outputted text file\n"	
+
+	#couldn't work out a working method to iterate this
+	A_count=$(echo $full_list | grep '\sA' | wc -l)
+	B_count=$(echo $full_list | grep '\sB' | wc -l)
+	C_count=$(echo $full_list | grep '\sC' | wc -l)
+	D_count=$(echo $full_list | grep '\sD' | wc -l)
+	E_count=$(echo $full_list | grep '\sE' | wc -l)
+	F_count=$(echo $full_list | grep '\sF' | wc -l)
+	
+	#setting up 2 arrays for the next nested loop
+	grade_array=('A' 'B' 'C' 'D' 'E' 'F')
+	count_array=($A_count $B_count $C_count $D_count $E_count $F_count)
+	len1=${#grade_array[@]}
+	len2=${#count_array[@]}
+
+	#this awkward bit of syntax will iterate through both arrays (I know they're the same length) and then
+	#provide an output as to how many instances each algorithm grade has
+	for ((i=1;i<$len1;i++))
+	do
+		for ((j=1;j<$len2;j++))
+		do
+			if [ $i -eq $j ]
+			then
+				echo "Remote host has" $count_array[j] "instances of" $grade_array[i] "grade cipher(s)"
+			fi
+		done
+	done
 
 }
+
 enum_ciphers(){
 	
 	#This test will use a combination of sslclient & nmap to determine the encryption algorithms used on the remote host
@@ -31,6 +61,8 @@ enum_ciphers(){
 	if [[ $failed_to_resolve_wc != 0 ]]
 	then
 		echo "Hostname invalid. Please check again & ensure that host is online"
+		#return 0 will end prematurely and ensure that the the function is exited before the next check takes place
+		return 0
 	else
 		false
 	fi	
@@ -45,8 +77,6 @@ enum_ciphers(){
 	else
 		echo_results
 	fi
-
-	#echo $full_list
 
 	if [[ $try_second_method == true ]]
 	then
